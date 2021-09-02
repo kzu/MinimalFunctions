2a5683c7bd2adb3e25c27f21e33e3927b02a60db
Daniel Cazzulino <daniel@cazzulino.com>
2021-09-02 17:18:27 -0300

    Switch to in-proc function

--- src/DefaultFunc/Person.cs
+++ src/DefaultFunc/Person.cs
@@ -1,14 +1,14 @@
 ﻿using System.Runtime.InteropServices;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 
 record Person(string FirstName, string LastName, string Runtime);
 
 record PersonApi(ILogger<Person> Log)
 {
-    [Function(nameof(GetPerson))]
-    public Person GetPerson([HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "person")] HttpRequestData req)
+    [FunctionName(nameof(GetPerson))]
+    public Person GetPerson([HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "person")] HttpRequestMessage req)
     {
         Log.LogInformation("We got DI too :)");
         return new Person("Daniel", "Cazzulino", RuntimeInformation.FrameworkDescription);
---
+++ src/DefaultFunc/Startup.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+public class Startup : FunctionsStartup
+{
+    public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder config) 
+    {
+        if (bool.TryParse(Environment.GetEnvironmentVariable("ATTACH_DEBUGGER"), out var attach) && attach)
+            Debugger.Launch();
+
+        var context = config.GetContext();
+        var env = context.EnvironmentName;
+        var appPath = context.ApplicationRootPath;
+
+        config.ConfigurationBuilder
+                .AddJsonFile(Path.Combine(appPath, "appsettings.json"), optional: true, reloadOnChange: false)
+                .AddDotNetConfig()
+                .AddJsonFile(Path.Combine(appPath, $"appsettings.{env}.json"), optional: true, reloadOnChange: true)
+                .AddJsonFile(Path.Combine(appPath, "local.settings.json"), optional: true, reloadOnChange: true);
+    }
+
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        // TODO: configure builder.Services
+    }
+}
