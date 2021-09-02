284dfeef527609f457e74d7c22ade218fa011348
Daniel Cazzulino <daniel@cazzulino.com>
2021-09-02 16:34:46 -0300

    Add local run and debug for minimal function

---
+++ src/DefaultFunc/Person.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+record Person(string FirstName, string LastName);
+
+record PersonApi(ILogger<Person> Log)
+{
+    [Function(nameof(GetPerson))]
+    public Person GetPerson([HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "person")] HttpRequestData req)
+    {
+        Log.LogInformation("We got DI too :)");
+        return new Person("Daniel", "Cazzulino");
+    }
+}
--- src/DefaultFunc/Program.cs
+++ src/DefaultFunc/Program.cs
@@ -1,9 +1,18 @@
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+
+var env = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") ?? "Development";
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureAppConfiguration(config =>
+    {
+        config.AddJsonFile(Path.Combine("appsettings.json"), optional: true, reloadOnChange: false)
+              .AddDotNetConfig()
+              .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
+              .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true);
+    })
     .ConfigureServices(s =>
     {
     })
@@ -12,10 +21,7 @@ var host = new HostBuilder()
     })
     .Build();
 
+if (bool.TryParse(Environment.GetEnvironmentVariable("ATTACH_DEBUGGER"), out var attach) && attach)
+    Debugger.Launch();
+
 await host.RunAsync();
-
-[Function("GetPerson")]
-Person GetPerson([HttpTrigger(AuthorizationLevel.Anonymous, "GET", "/person")] HttpRequestData req, FunctionContext context) 
-    => new Person("Daniel", "Cazzulino");
-
-record Person(string FirstName, string LastName);
