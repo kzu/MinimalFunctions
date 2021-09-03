a14ffd1b35ca07fe7a2d54fff0ddb3f73c9c5bbb
Daniel Cazzulino <daniel@cazzulino.com>
2021-09-03 08:55:00 -0300

    Show app version in output too

--- src/MinimalFunction/Person.cs
+++ src/MinimalFunction/Person.cs
@@ -3,7 +3,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 
-record Person(string FirstName, string LastName, string Runtime);
+record Person(string FirstName, string LastName, string Runtime, string Version);
 
 record PersonApi(ILogger<Person> Log)
 {
@@ -11,6 +11,6 @@ record PersonApi(ILogger<Person> Log)
     public Person GetPerson([HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "person")] HttpRequestMessage req)
     {
         Log.LogInformation("We got DI too :)");
-        return new Person("Daniel", "Cazzulino", RuntimeInformation.FrameworkDescription);
+        return new Person("Daniel", "Cazzulino", RuntimeInformation.FrameworkDescription, ThisAssembly.Info.InformationalVersion);
     }
 }
